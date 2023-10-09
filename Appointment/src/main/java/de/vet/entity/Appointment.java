b68a23c4f92f1379d998b6a7fa472c92265d5c3f
lars <lars.hendrik.diekmann@gmail.com>
2023-10-09 12:38:31 +0200

    init

---
+++ Appointment/src/main/java/de/vet/entity/Appointment.java
@@ -0,0 +1,87 @@
+package de.vet.entity;
+
+import jakarta.persistence.*;
+
+//author Lars Diekmann
+@Entity
+@Table(name="Appointment")
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "appointmentID")
+    private long appointmentID;
+    private String date;
+    private int priceVariant;
+    //private picture?
+    private String diagnose;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "got", referencedColumnName = "gotID")
+    private GOT got;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "drug", referencedColumnName = "drugID")
+    private Drug drug;
+
+    public Appointment(){}
+    public Appointment(long appointmentID, String date, int priceVariant, String diagnose){
+       this.appointmentID = appointmentID;
+       this.date = date;
+       if(isVariantValid(priceVariant)){
+           this.priceVariant = priceVariant;
+
+       } else{
+           this.priceVariant = 1;
+       }
+       this.diagnose = diagnose;
+    }
+
+    public Appointment(String date, int priceVariant, String diagnose){
+        this.date = date;
+        if(isVariantValid(priceVariant)){
+            this.priceVariant = priceVariant;
+
+        } else{
+            this.priceVariant = 1;
+        }
+        this.diagnose = diagnose;
+    }
+
+    private boolean isVariantValid(int priceVariant){
+        if(priceVariant == 1 || priceVariant == 2 || priceVariant == 3){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public long getAppointmentID() {
+        return appointmentID;
+    }
+
+    public void setAppointmentID(long appointmentID) {
+        this.appointmentID = appointmentID;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getPriceVariant() {
+        return priceVariant;
+    }
+
+    public void setPriceVariant(int priceVariant) {
+        this.priceVariant = priceVariant;
+    }
+
+    public String getDiagnose() {
+        return diagnose;
+    }
+
+    public void setDiagnose(String diagnose) {
+        this.diagnose = diagnose;
+    }
+}
---
+++ Appointment/src/main/java/de/vet/entity/GOT.java
@@ -0,0 +1,4 @@
+package de.vet.entity;
+
+public class GOT {
+}
